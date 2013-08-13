dd4777c20b28304d7097dded4aaa8a55cb0f4776
Your Name <you@example.com>
2013-08-13 17:22:17 +0400

    sdfsdf

--- check.php
+++ check.php
@@ -2,8 +2,8 @@
 <?php
 error_reporting(0);
 
-$api_login      = "storage_5591_16";
-$api_key        = "4f6a19c0df32cb5ffb92d297e3592edd";
+$api_login      = "api_user";
+$api_key        = "api_key";
 
 include_once ("./php-cloudfiles/cloudfiles.php");
 
@@ -11,23 +11,46 @@ $auth = new CF_Authentication($api_login, $api_key);
 $auth->authenticate();
 $conn = new CF_Connection($auth);
 
-$a = $conn->get_container('public')->get_objects();
+print "connection to storage\t";
+if(($a = $conn->get_containers())) print "ok"; else print "fail";
 
-foreach($a as $l)
+foreach($a as $n)
 {
+#    if($n->name == 'private') continue;
 
-    $c = curl_init();
-    $r = curl_setopt($c, CURLOPT_URL,            'http://' . str_replace('_', '-', $api_login) . '.cs.clodoserver.ru/' . $l->name);
-    $r = curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    $r = curl_setopt($c, CURLOPT_TIMEOUT,        30);
-    $r = curl_exec($c);
-    $i = curl_getinfo($c);
-    curl_close($c);
-
-    if($i['http_code'] > 350)
+    print "\nentering in container {$n->name}\t";
+    #if(($e = $conn->get_container('public')))
+    if(($e = $conn->get_container($n->name)))
     {
-        print $i['http_code'] . ' ';
-        print $l->name . "\n";
-    }
-}
+        print "ok\nget objects\t";
+	if(($e = $e->get_objects()))
+        {
 
+            foreach($e as $l)
+	    {
+                print "\n" . $l->name . "\t";
+                $u = 'http://' . str_replace('_', '-', $api_login) . '.cs.clodoserver.ru/' . $n->name . '/' . $l->name;
+                $c = curl_init();
+                $r = curl_setopt($c, CURLOPT_URL,            $u);
+                $r = curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+                $r = curl_setopt($c, CURLOPT_TIMEOUT,        30);
+                $r = curl_exec($c);
+                $i = curl_getinfo($c);
+                curl_close($c);
+
+                if($i['http_code'] > 350)
+                {
+                    print 'error - ' . $i['http_code'] . ' ';
+                    print $u;
+		}
+                else
+		{
+                    print "ok";
+                }
+	    }
+	}
+        else print "fail";
+    }
+    else print "fail";
+    print "\n";
+}
