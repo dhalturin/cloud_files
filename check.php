7f14c77676111c9b191664bab60d041a5804d1e5
Your Name <you@example.com>
2013-08-13 16:48:30 +0400

    fsdf

---
+++ check.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+error_reporting(0);
+
+$api_login      = "storage_5591_16";
+$api_key        = "4f6a19c0df32cb5ffb92d297e3592edd";
+
+include_once ("./php-cloudfiles/cloudfiles.php");
+
+$auth = new CF_Authentication($api_login, $api_key);
+$auth->authenticate();
+$conn = new CF_Connection($auth);
+
+$a = $conn->get_container('public')->get_objects();
+
+foreach($a as $l)
+{
+
+    $c = curl_init();
+    $r = curl_setopt($c, CURLOPT_URL,            'http://' . str_replace('_', '-', $api_login) . '.cs.clodoserver.ru/' . $l->name);
+    $r = curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    $r = curl_setopt($c, CURLOPT_TIMEOUT,        30);
+    $r = curl_exec($c);
+    $i = curl_getinfo($c);
+    curl_close($c);
+
+    if($i['http_code'] > 350)
+    {
+        print $i['http_code'] . ' ';
+        print $l->name . "\n";
+    }
+}
+
